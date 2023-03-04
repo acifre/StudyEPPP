a564fee47af28fbde67a5124ffe85b4c904e0c81
acifre <anthonybcifre@gmail.com>
2023-03-04 14:00:11 -0600

    Initial Commit

---
+++ StudyEPPP/StudyEPPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyEPPPApp.swift
+//  StudyEPPP
+//
+//  Created by Anthony Cifre on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyEPPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
